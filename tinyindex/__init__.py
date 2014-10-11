4535b46ff2016b36ef9551077a7cd249da2ad2f0
Eugene Eeo <xe0jun@hotmail.com>
2014-10-11 08:12:12 +0800

    some meta information

--- tinyindex/__init__.py
+++ tinyindex/__init__.py
@@ -1,1 +1,12 @@
+"""
+    tinyindex
+    ~~~~~~~~~
+
+    Provides document indexing for TinyDB, and
+    ensures deterministic yielding of documents.
+
+    :copyright: (c) 2014 by Eugene Eeo
+    :license: MIT, see LICENSE for more details
+"""
+
 from tinyindex.index import Index
